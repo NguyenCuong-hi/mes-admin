5dc04a9b0fd85c4f35cae4c3f15608945b434c44
ADMIN\nguye <nvc.nvcuong@gmail.com>
2024-07-21 17:33:08 +0700

    init project

---
+++ src/main/java/com/example/mesadmin/MesAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.mesadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MesAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mesadmin/MesAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mesadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
